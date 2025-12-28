41f8907c56d91c3d573f49d8df748f3c3f76cf65
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2025-12-28 21:09:20 +0530

    add new

---
+++ count words.c
@@ -0,0 +1,43 @@
+//space remove
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+int a[200],b[200];
+printf("Enter your string");
+scanf("%s",&a[200]);
+int j=0;
+for(int i=0;a[i]!='\0';i++){
+        if(a[i]==' '){
+          b[i]==a[i];
+          j++;
+        }
+    b[j]='\0';
+}
+printf("%s",b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
