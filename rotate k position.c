cba1fb47566b9973187cface15222c8359fe4c0f
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-01-09 11:15:12 +0530

    add new

---
+++ rotate k position.c
@@ -0,0 +1,60 @@
+//k jetlo position thi matrix ne fervavano
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int n,k;
+    printf("Enter your string: ");
+    scanf("%s",&n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
