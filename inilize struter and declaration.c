5d1d086a35bff8276cb346c94bf7a28d8aee13da
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-01-10 14:11:30 +0530

    structure practice

---
+++ inilize struter and declaration.c
@@ -0,0 +1,30 @@
+//aeki sathe line ma bdha declare thy jase
+
+#include<stdio.h>
+struct iteam{
+    char name[10];
+    int pin;
+    float number;
+
+};
+
+void main(){
+struct iteam a1={"dhruti",333,99.56};
+
+printf("%s\n",a1.name);
+printf("%d\n",a1.pin);
+printf("%f\n",a1.number);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
