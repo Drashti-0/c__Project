663f19754e20b51a3a591412ef8b39f5ba7cda19
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-01-13 15:34:16 +0530

    add new

--- factorial.c
+++ factorial.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
-int ffunction(int num){
+int main(){
+
+int i,facto,n;
+printf("Enter your number: ");
+scanf("%d",&n);
+
+facto=1;
+for(i=1;i<=n;i++){
+    facto*=i;
+}
+
+
+printf("%d",facto);
 
 
 
