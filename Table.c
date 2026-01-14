9fbf3be0088b736d5ba3e98be0d43b8cf6e9a31a
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-01-14 07:36:33 +0530

    add new c file

---
+++ Table.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main(){
+
+int i,n,table;
+
+printf("Enter your number: ");
+scanf("%d",&n);
+
+for(i=1;i<=10;i++){
+    printf("%d X %d = %d\n",n,i,n*i);
+}
+
+
+
+
+
+
+}
