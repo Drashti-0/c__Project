5ffd12d06c43264dee8ee007fc62f91c5c392e6c
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-02-04 23:03:53 +0530

    function practice with c

--- factorial.c
+++ factorial.c
@@ -1,17 +1,5 @@
 #include<stdio.h>
-int main(){
-
-int i,facto,n;
-printf("Enter your number: ");
-scanf("%d",&n);
-
-facto=1;
-for(i=1;i<=n;i++){
-    facto*=i;
-}
-
-
-printf("%d",facto);
+int ffunction(int num){
 
 
 
